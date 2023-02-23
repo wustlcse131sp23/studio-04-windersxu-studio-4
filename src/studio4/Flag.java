77357e3ff1bbba6bc37b6745725427f24161fb34
xuzhixin <xuzhixin@localhost>
2023-02-23 14:15:07 -0600

    Studio 4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,24 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		Color orange = new Color(255, 151, 0);
+		StdDraw.setPenColor(orange);
+		StdDraw.filledRectangle(.2,.5,.2 ,.3); 
+		
+		Color blue = new Color(50, 50, 255);
+		StdDraw.setPenColor(blue);
+		StdDraw.filledRectangle(.6, .5, .2, .3);
+		//StdDraw.setPenColor(Color.BLUE);
+		//StdDraw.filledCircle(.4, .5, .15);
+		
+		//StdDraw.setPenColor(orange);
+		//StdDraw.filledRectangle(.4, .5, .15, .15);
+		
+		//StdDraw.setPenColor(blue);
+		//StdDraw.filledRectangle(.325, .5, .15/2, .15);
+		
+		StdDraw.setPenColor(Color.PINK);
+		StdDraw.filledCircle(.4, .5, .2);
+		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -20,5 +21,32 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String a = in.next();
+		int b = in.nextInt();
+		int c = in.nextInt();
+		int d = in.nextInt();
+		boolean e = in.nextBoolean();
+		double x = in.nextDouble();
+		double y = in.nextDouble();
+		double halfwidth = in.nextDouble();
+		double halflength = in.nextDouble();
+		System.out.print(a +" " + b +" " + c + " " + d + " " + e + " " + x + " " + y);
+		
+		if (a.equals ("rectangle")) {
+			Color black = new Color(b, c, d);
+			StdDraw.setPenColor(black);
+			if (e == true) {
+				StdDraw.filledRectangle(x, y,halfwidth, halflength); 
+			} else {
+				StdDraw.rectangle(x, y,halfwidth, halflength);
+			}
+			
+		} else if (a.equals ("ellipse")) {
+			Color rgb = new Color(b,c,d);
+			StdDraw.setPenColor(rgb);
+			StdDraw.filledEllipse(x, y,halfwidth, halflength);
+		}
+		
+
 	}
 }
